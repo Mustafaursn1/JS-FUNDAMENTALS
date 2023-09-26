e288a4caec32da251778abd32372342dc43937f2
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-09-26 21:23:11 +0200

    first commit about js variable

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -18,5 +18,13 @@ console.log(age)
 
 //constants 
 const occupation ="Engineer"
-occupation="driver"
+occupation
 console.log(occupation)
+
+//data types
+var middleName="David"  //string  and 'David'
+var ageOfBrother=25  //number  if i write '25' -->stirng 
+var isHeMarried=false //boolean true or false
+var yearsInMarriage=null// null data typ ecpected result not having 
+var numberOfCars=undefined // undefined  
+

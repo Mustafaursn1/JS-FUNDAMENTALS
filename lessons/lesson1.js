996c3361814e6105102dbb135fcee8a928348784
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-09-26 17:39:46 +0200

    first commit by variable

---
+++ lessons/lesson1.js
@@ -0,0 +1,22 @@
+//1.Hello World
+console.log("Hello World!")
+
+
+//variables
+var firstName = "John"
+let lastname="Smith"
+console.log(firstName)
+console.log(lastname)
+
+var age , dateOfBirth, sex
+
+age="5"
+sex="Male"
+console.log(age)
+age="6"
+console.log(age)
+
+//constants 
+const occupation ="Engineer"
+occupation="driver"
+console.log(occupation)

8d18b395c4218766293f26636fb8f66a96e3344c
Mustafaursn1 <69422949+Mustafaursn1@users.noreply.github.com>
2023-09-28 19:25:49 +0200

    Function Type

---
+++ lessons/lesson8.js
@@ -0,0 +1,37 @@
+//Declaretive function
+
+function helloOne (){
+    console.log('Hello one!')
+}
+helloOne()
+
+//Anoymus fuction
+//ikisi arasindaki fark Declaretive function dan önce helloOne() calisir
+//Anoynus 'dan önce helloTwo() calismaz ve hata aliriz
+var helloTwo =function(){
+    console.log('Hello two!')
+
+}
+helloTwo()
+
+//ES6 function syntax or arrow function
+
+var helloTree = () => {
+    console.log('Hello Three!')
+}
+helloTree()
+
+//Function with arguments
+function printName(name, lastName){
+    console.log(name+' '+lastName)
+
+}
+printName('Mike','Smith')
+
+//Function with return
+function multiplyByTwo(number){
+    var result =number * 2
+    return result
+}
+var myResult=multiplyByTwo(5)
+console.log(myResult)
